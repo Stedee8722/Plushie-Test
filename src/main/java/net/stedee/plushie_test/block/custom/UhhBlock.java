962908cae3a3e4f45aa3e2cdd6882e674d5ec2fc
Stedee <doanthanhbnh@gmail.com>
2024-06-06 23:04:30 +0700

    Small change to a block, makes it directional

--- src/main/java/net/stedee/plushie_test/block/custom/UhhBlock.java
+++ src/main/java/net/stedee/plushie_test/block/custom/UhhBlock.java
@@ -7,10 +7,14 @@ import javax.annotation.Nullable;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition.Builder;
 
-public class UhhBlock extends Block {
+public class UhhBlock extends HorizontalDirectionalBlock {
 
     public UhhBlock(Properties pProperties) {
         super(pProperties);
@@ -24,4 +28,16 @@ public class UhhBlock extends Block {
         super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 
+    @SuppressWarnings("null")
+    @Override
+    protected void createBlockStateDefinition(Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
 }
