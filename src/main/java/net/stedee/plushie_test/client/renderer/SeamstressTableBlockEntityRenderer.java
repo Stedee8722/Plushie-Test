e33b13ab5132198a04ff3ba67f32a92a2359419f
Stedee <doanthanhbnh@gmail.com>
2024-09-22 15:14:22 +0700

    Added renderer for Seamstress Table block, bump to 0.4.1

--- src/main/java/net/stedee/plushie_test/client/renderer/SeamstressTableBlockEntityRenderer.java
+++ src/main/java/net/stedee/plushie_test/client/renderer/SeamstressTableBlockEntityRenderer.java
@@ -1,5 +1,12 @@
 package net.stedee.plushie_test.client.renderer;
 
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LightLayer;
+import net.stedee.plushie_test.recipe.custom.SeamstressRecipe;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Quaternionf;
 
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -26,7 +33,7 @@ import net.stedee.plushie_test.config.ClientConfig;
 public class SeamstressTableBlockEntityRenderer implements BlockEntityRenderer<SeamstressTableBlockEntity> {
 
     private final float _blockScale = 0.25f;
-    private final float _itemScale = 0.125f;
+    private final float _itemScale = 0.126f;
     private final BlockEntityRendererProvider.Context context;
 
     public SeamstressTableBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
@@ -35,8 +42,8 @@ public class SeamstressTableBlockEntityRenderer implements BlockEntityRenderer<S
 
     @SuppressWarnings("null")
     @Override
-    public void render(SeamstressTableBlockEntity blockEntity, float var2, PoseStack matrixStack,
-            MultiBufferSource iRenderTypeBuffer, int light, int var6) {
+    public void render(@NotNull SeamstressTableBlockEntity blockEntity, float pPartialTick, @NotNull PoseStack matrixStack,
+                       @NotNull MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
         ClientConfig config = AutoConfig.getConfigHolder(ClientConfig.class).getConfig();
         //try(Level level = blockEntity.getLevel()) {
         //    isClient = level.isClientSide;
@@ -46,58 +53,144 @@ public class SeamstressTableBlockEntityRenderer implements BlockEntityRenderer<S
         if (!config.displayitemsintable || blockEntity.input.isEmpty())// || !isClient) 
             return;
 
-        BlockState state = blockEntity.getBlockState();
+        ItemStack stack1 = blockEntity.getInventory().getStackInSlot(0);
+        ItemStack stack2 = blockEntity.getInventory().getStackInSlot(1);
+        ItemStack stack_out = blockEntity.getInventory().getStackInSlot(2);
 
-        double height = state.hasProperty(SlabBlock.TYPE) ? state.getValue(SlabBlock.TYPE) == SlabType.BOTTOM ?.5:1:1;
-        // get type of block
+        if (stack1.isEmpty() && stack2.isEmpty())
+            return;
 
-        final double spacing = .189;
-        final double offset = .31;
-        //translate x,y,z
-        matrixStack.translate(0,.0625 + height, 0);
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 2; j++) {
-                    ItemStack item = blockEntity.input.getStackInSlot(j + 3 * i);
-                    if (item.isEmpty()) continue;
+        Level level = blockEntity.getLevel();
+        if (level == null)
+            return;
 
-                    boolean isItem = !(item.getItem() instanceof BlockItem);
+        BlockPos pos = blockEntity.getBlockPos().above();
 
-                    //pushmatrix
-                    matrixStack.pushPose();
-                    matrixStack.translate(0.5, 0.5, 0.5);
-                    matrixStack.mulPose((new Quaternionf()).rotationY(getRotation(state.getValue(SeamstressTableBlock.FACING)) * ((float)Math.PI / 180F)));
-                    matrixStack.translate(-0.5, -0.5, -0.5);
-                    //translate x,y,z (invert direction of j to correct the positioning on the table)
-                    matrixStack.translate(spacing * i + offset, isItem ? -0.05 : 0, spacing * (2-j) + offset);
-                    matrixStack.mulPose((new Quaternionf()).rotationY(270 * ((float)Math.PI / 180F)));
-                    //scale x,y,z
-                    float scale = isItem ? _itemScale : _blockScale;
-                    matrixStack.scale(scale, scale, scale);
+        //render first slot
+        matrixStack.pushPose();
+        matrixStack.translate(0.5, 0, 0.5);
+        matrixStack.rotateAround((new Quaternionf()).rotationY(getRotation(blockEntity.getBlockState().getValue(SeamstressTableBlock.FACING)) * ((float)Math.PI / 180F)), (float)0, (float)0, (float)0);
+        matrixStack.translate(-0.5, 0, -0.5);
+        matrixStack.translate(0.245, 1.05, 0.245);
+        matrixStack.scale(_itemScale, _itemScale, _itemScale);
+        matrixStack.mulPose(Axis.XP.rotationDegrees(90));
+        matrixStack.mulPose(Axis.ZP.rotationDegrees(180));
+        this.context.getItemRenderer().renderStatic(
+                stack1,
+                ItemDisplayContext.FIXED,
+                LightTexture.pack(
+                        level.getBrightness(LightLayer.BLOCK, pos),
+                        level.getBrightness(LightLayer.SKY, pos)
+                ),
+                OverlayTexture.NO_OVERLAY,
+                matrixStack,
+                pBuffer,
+                level,
+                0
+        );
+        matrixStack.popPose();
 
-                    if (isItem) {
-                    matrixStack.mulPose((new Quaternionf()).rotationX(90 * ((float)Math.PI / 180F)));
-                    }
+        //render second slot
+        matrixStack.pushPose();
+        matrixStack.translate(0.5, 0, 0.5);
+        matrixStack.rotateAround((new Quaternionf()).rotationY(getRotation(blockEntity.getBlockState().getValue(SeamstressTableBlock.FACING)) * ((float)Math.PI / 180F)), (float)0, (float)0, (float)0);
+        matrixStack.translate(-0.5, 0, -0.5);
+        matrixStack.translate(0.755, 1.05, 0.245);
+        matrixStack.scale(_itemScale, _itemScale, _itemScale);
+        matrixStack.mulPose(Axis.XP.rotationDegrees(90));
+        matrixStack.mulPose(Axis.ZP.rotationDegrees(180));
+        this.context.getItemRenderer().renderStatic(
+                stack2,
+                ItemDisplayContext.FIXED,
+                LightTexture.pack(
+                        level.getBrightness(LightLayer.BLOCK, pos),
+                        level.getBrightness(LightLayer.SKY, pos)
+                ),
+                OverlayTexture.NO_OVERLAY,
+                matrixStack,
+                pBuffer,
+                level,
+                0
+        );
+        matrixStack.popPose();
 
-                    BakedModel bakedmodel = Minecraft.getInstance().getItemRenderer()
-                            .getModel(item, blockEntity.getLevel(), null, 0);
+        //render result slot
+        matrixStack.pushPose();
+        matrixStack.translate(0.5, 0, 0.5);
+        matrixStack.rotateAround((new Quaternionf()).rotationY(getRotation(blockEntity.getBlockState().getValue(SeamstressTableBlock.FACING)) * ((float)Math.PI / 180F)), (float)0, (float)0, (float)0);
+        matrixStack.translate(-0.5, 0, -0.5);
+        matrixStack.translate(0.5, 1.05, 0.7);
+        matrixStack.scale(_itemScale, _itemScale, _itemScale);
+        matrixStack.mulPose(Axis.XP.rotationDegrees(90));
+        matrixStack.mulPose(Axis.ZP.rotationDegrees(180));
+        this.context.getItemRenderer().renderStatic(
+                stack_out,
+                ItemDisplayContext.FIXED,
+                LightTexture.pack(
+                        level.getBrightness(LightLayer.BLOCK, pos),
+                        level.getBrightness(LightLayer.SKY, pos)
+                ),
+                OverlayTexture.NO_OVERLAY,
+                matrixStack,
+                pBuffer,
+                level,
+                0
+        );
+        matrixStack.popPose();
 
 
-                    int lightAbove = LevelRenderer.getLightColor(blockEntity.getLevel(), blockEntity.getBlockPos().above());
-                    Minecraft.getInstance().getItemRenderer().render(item, ItemDisplayContext.FIXED,
-                                false, matrixStack, iRenderTypeBuffer, lightAbove, OverlayTexture.NO_OVERLAY, bakedmodel);
-                    //popmatrix
-                    matrixStack.popPose();
-            }
-        }
+        //BlockState state = blockEntity.getBlockState();
+//
+        //double height = state.hasProperty(SlabBlock.TYPE) ? state.getValue(SlabBlock.TYPE) == SlabType.BOTTOM ?.5:1:1;
+        //// get type of block
+//
+        //final double spacing = .189;
+        //final double offset = .31;
+        ////translate x,y,z
+        //matrixStack.translate(0,.0625 + height, 0);
+        //for (int i = 0; i < 1; i++) {
+        //    for (int j = 0; j < 2; j++) {
+        //            ItemStack item = blockEntity.input.getStackInSlot(j + 3 * i);
+        //            if (item.isEmpty()) continue;
+//
+        //            boolean isItem = !(item.getItem() instanceof BlockItem);
+//
+        //            //pushmatrix
+        //            matrixStack.pushPose();
+        //            matrixStack.translate(0.5, 0.5, 0.5);
+        //            matrixStack.mulPose((new Quaternionf()).rotationY(getRotation(state.getValue(SeamstressTableBlock.FACING)) * ((float)Math.PI / 180F)));
+        //            matrixStack.translate(-0.5, -0.5, -0.5);
+        //            //translate x,y,z (invert direction of j to correct the positioning on the table)
+        //            matrixStack.translate(spacing * i + offset, isItem ? -0.05 : 0, spacing * (2-j) + offset);
+        //            matrixStack.mulPose((new Quaternionf()).rotationY(270 * ((float)Math.PI / 180F)));
+        //            //scale x,y,z
+        //            float scale = isItem ? _itemScale : _blockScale;
+        //            matrixStack.scale(scale, scale, scale);
+//
+        //            if (isItem) {
+        //            matrixStack.mulPose((new Quaternionf()).rotationX(90 * ((float)Math.PI / 180F)));
+        //            }
+//
+        //            BakedModel bakedmodel = Minecraft.getInstance().getItemRenderer()
+        //                    .getModel(item, blockEntity.getLevel(), null, 0);
+//
+//
+        //            int lightAbove = LevelRenderer.getLightColor(blockEntity.getLevel(), blockEntity.getBlockPos().above());
+        //            Minecraft.getInstance().getItemRenderer().render(item, ItemDisplayContext.FIXED,
+        //                        false, matrixStack, iRenderTypeBuffer, lightAbove, OverlayTexture.NO_OVERLAY, bakedmodel);
+        //            //popmatrix
+        //            matrixStack.popPose();
+        //    }
+        //}
     }
 
     private int getRotation(Direction dir) {
         switch (dir) {
-          case NORTH -> {return 270;}
-          case EAST  -> {return 180;}
-          case SOUTH -> {return 90;}
-          case WEST  -> {return 0;}
-          default -> {return 0;}
+          case NORTH -> {return 180;}
+          case EAST  -> {return 90;}
+          case SOUTH -> {return 0;}
+          case WEST  -> {return 270;}
+          default -> {return -1;}
         }
     }
 }
--- src/main/java/net/stedee/plushie_test/inventory/custom/Seamstress/SeamstressTableMenu.java
+++ src/main/java/net/stedee/plushie_test/inventory/custom/Seamstress/SeamstressTableMenu.java
@@ -138,7 +138,6 @@ public class SeamstressTableMenu extends AbstractContainerMenu {
                 return ItemStack.EMPTY;
             }
         } else {
-            System.out.println("Invalid slotIndex:" + pIndex);
             return ItemStack.EMPTY;
         }
         // If stack size == 0 (the entire stack was moved) set slot contents to null
