752855eb9b71640432c76344e407fc72c9693440
Stedee <doanthanhbnh@gmail.com>
2024-10-26 21:52:53 +0700

    Make it so Glaceon mask makes the player invincible to snow powder

---
+++ src/main/java/net/stedee/plushie_test/mixin/PlayerMixin.java
@@ -0,0 +1,24 @@
+package net.stedee.plushie_test.mixin;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameRules;
+import net.stedee.plushie_test.item.ModdedItems;
+import net.stedee.plushie_test.plushie_test;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+
+    @Shadow public abstract Inventory getInventory();
+
+    @Redirect(method = "isInvulnerableTo(Lnet/minecraft/world/damagesource/DamageSource;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z", ordinal = 3))
+    public boolean moddedIsVulnerableto(GameRules instance, GameRules.Key<GameRules.BooleanValue> pKey) {
+        plushie_test.LOGGER.debug("Injected");
+        PlayerMixin player = (PlayerMixin) (Object) this;
+        return instance.getBoolean(pKey) && !player.getInventory().getArmor(3).is(ModdedItems.MASK_GLACEON.get());
+    }
+}
