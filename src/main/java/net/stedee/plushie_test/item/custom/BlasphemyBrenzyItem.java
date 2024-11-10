d4a6229d16241aea5651577eb01cbdee44744239
Stedee <doanthanhbnh@gmail.com>
2024-11-11 01:57:16 +0700

    fixed for BlasphemyBrenzyItem too

--- src/main/java/net/stedee/plushie_test/item/custom/BlasphemyBrenzyItem.java
+++ src/main/java/net/stedee/plushie_test/item/custom/BlasphemyBrenzyItem.java
@@ -1,6 +1,5 @@
 package net.stedee.plushie_test.item.custom;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.IntTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
@@ -10,12 +9,13 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.*;
 import net.minecraftforge.common.ForgeMod;
 import net.stedee.plushie_test.enchantment.ModdedEnchantments;
 import net.stedee.plushie_test.entity.custom.BlasphemyBrenzyProjectile;
@@ -40,13 +40,34 @@ public class BlasphemyBrenzyItem extends BlasphemyItem {
 
     @Override
     public boolean onEntitySwing(ItemStack stack, LivingEntity entity) {
-        if ((Minecraft.getInstance().hitResult != null ? Minecraft.getInstance().hitResult.getType() : null) != HitResult.Type.MISS) {
+        if (!(entity instanceof Player)) { return false;}
+        Vec3 from = entity.getEyePosition(1F);
+        Vec3 look = entity.getViewVector(1F);
+
+        // see if can reach entities
+        double d0 = ((Player) entity).getBlockReach();
+        double entityReach = ((Player) entity).getEntityReach();
+        double d1;
+        if (((Player)entity).isCreative()) {
+            d0 = 6.0D;
+        }
+        d0 = d1 = Math.max(d0, entityReach);
+        d1 *= d1;
+        Vec3 vec32 = from.add(look.x * d0, look.y * d0, look.z * d0);
+        AABB aabb = entity.getBoundingBox().expandTowards(look.scale(d0)).inflate(1.0D, 1.0D, 1.0D);
+        EntityHitResult entityhitresult = ProjectileUtil.getEntityHitResult(entity, from, vec32, aabb, (p_234237_) -> !p_234237_.isSpectator() && p_234237_.isPickable(), d1);
+
+        // see if can reach blocks
+        double blockReachDistance = ((Player)entity).getBlockReach();
+        Vec3 to = from.add(look.x * blockReachDistance, look.y * blockReachDistance, look.z * blockReachDistance);
+        BlockHitResult blockhitresult = entity.level().clip(new ClipContext(from, to, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, entity));
+
+        if ((blockhitresult.getType() != HitResult.Type.MISS) || (entityhitresult != null ? entityhitresult.getType() != HitResult.Type.MISS : false)) {
             return super.onEntitySwing(stack, entity);
         }
         if (!entity.level().isClientSide()) {
             float pDamage = eDamage * (EnchantmentHelper.getEnchantmentLevel(ModdedEnchantments.BLASPHEMY_DAMAGE.get(), entity) + 1);
 
-            Vec3 look = entity.getViewVector(1F);
             Entity blasphemy = super.makeProjectile(new BlasphemyBrenzyProjectile(entity.level(), entity, 0, 0, 0, pDamage * ((Player) entity).getAttackStrengthScale(0)), entity, look);
 
             entity.level().addFreshEntity(blasphemy);
